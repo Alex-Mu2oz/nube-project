89908a4ffc96153c425394b13190ffa1466f757c
Alex-Mu2oz <105660723+Alex-Mu2oz@users.noreply.github.com>
2024-08-31 06:28:06 -0500

    commit

---
+++ nube_2024-08-30_212302/.next/static/chunks/app/monitoring/page-48268cacbaee2be2.js
@@ -0,0 +1,9 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[738],{251:function(e,r,t){Promise.resolve().then(t.bind(t,6093))},6093:function(e,r,t){"use strict";t.r(r);var n=t(7437),s=t(2265);r.default=()=>{let[e,r]=(0,s.useState)(new Date);return(0,s.useEffect)(()=>{let e=setInterval(()=>{r(new Date)},2e3);return()=>{clearInterval(e)}},[]),(0,n.jsxs)("section",{children:[(0,n.jsxs)("div",{style:{textAlign:"center",marginTop:"20px"},suppressHydrationWarning:!0,children:["\xdaltima actualizaci\xf3n: ",e.toLocaleTimeString()]}),(0,n.jsx)("iframe",{src:"/proxy",width:"100%",height:"500px",title:"iframe",suppressHydrationWarning:!0},e.valueOf())]})}},622:function(e,r,t){"use strict";/**
+ * @license React
+ * react-jsx-runtime.production.min.js
+ *
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */var n=t(2265),s=Symbol.for("react.element"),i=Symbol.for("react.fragment"),o=Object.prototype.hasOwnProperty,a=n.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner,u={key:!0,ref:!0,__self:!0,__source:!0};function q(e,r,t){var n,i={},f=null,l=null;for(n in void 0!==t&&(f=""+t),void 0!==r.key&&(f=""+r.key),void 0!==r.ref&&(l=r.ref),r)o.call(r,n)&&!u.hasOwnProperty(n)&&(i[n]=r[n]);if(e&&e.defaultProps)for(n in r=e.defaultProps)void 0===i[n]&&(i[n]=r[n]);return{$$typeof:s,type:e,key:f,ref:l,props:i,_owner:a.current}}r.Fragment=i,r.jsx=q,r.jsxs=q},7437:function(e,r,t){"use strict";e.exports=t(622)}},function(e){e.O(0,[971,472,744],function(){return e(e.s=251)}),_N_E=e.O()}]);
